b5edcf5a729d9369d958f61ed5c037d5df77a7d9
smukher6-stack <smukher6@pratt.edu>
2026-03-12 00:31:50 -0400

    gonna do this when i wake up tomorrow, god im tired

--- walking sim/Assets/scripting/Itema.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Itema : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public int Item;
-}
---
+++ walking sim/Assets/scripting/cutsceneScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Timeline;
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(SignalReceiver))]
+public class cutsceneScript : MonoBehaviour
+{
+    [SerializeField] public GameObject _cutsceneManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
